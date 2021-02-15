4ff67112f66c641eeb946ef712968268ea85ecd9
Колчин Леонид Евгеньевич <lekolchin@sberbank.ru>
2021-02-15 23:49:40 +0300

    remove getUserProfile method

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,10 +1,1 @@
-import * as prod from './user';
-import * as dev from './fake';
-
-export const getUserProfile = (() => {
-  if (process.env.REACT_APP_DEV_MODE === 'true') {
-    return dev.getUserProfile;
-  } else {
-    return prod.getUserProfile;
-  }
-})();
+export * from './user';
